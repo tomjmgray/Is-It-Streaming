28f7fb83b1031bde5e8b886a9cb82c97653e8d56
tomjmgray <tomjmgray96@gmail.com>
2021-02-05 11:21:45 -0800

    adds some rendering to search template, begins process for authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('films/', views.films, name="films"),
-    path('services/', views.services, name="services")
+    path('services/', views.services, name="services"),
+    path('search_movie/', views.search_movie, name="search_movie")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie, Service
+from . import urls
 
 # Create your views here.
 def home(request):
@@ -16,4 +17,17 @@ def services(request):
     context = { 'services': services }
     print(services)
     return render(request, 'services.html', context)
+
+def search_movie(request):
+    query = request.POST['query']
+    movies = Movie.objects.filter(title__contains=query)
+    if movies:
+        return render(request, 'search_results.html', {'movies': movies})
+
+    
+    # query = request.POST.query
+    # results = Movie.objects.filter(name__icontains=query)
+    # context = {'results': results}
+    # return render(request, 'search_results', context)
+
         
